47f46d27d26e3793f45b8593be064a2d6f07a04c
vinupillai14 <radhakrishnavinu@gmail.com>
2026-02-28 23:08:21 +0000

    fix: remove availability zones (not supported in eastus)

--- DevOps/terraform/aks/main.tf
+++ DevOps/terraform/aks/main.tf
@@ -9,7 +9,6 @@ resource "azurerm_kubernetes_cluster" "this" {
     name            = var.default_node_pool_name
     node_count      = var.enable_auto_scaling ? null : var.default_node_pool_count
     vm_size         = var.default_node_pool_vm_size
-    zones           = var.availability_zones
     max_pods        = var.max_pods
     os_disk_size_gb = var.os_disk_size_gb
     min_count       = var.enable_auto_scaling ? var.min_count : null
@@ -35,7 +34,6 @@ resource "azurerm_kubernetes_cluster_node_pool" "additional" {
   kubernetes_cluster_id = azurerm_kubernetes_cluster.this.id
   node_count            = var.additional_node_pools[count.index].enable_auto_scaling ? null : var.additional_node_pools[count.index].node_count
   vm_size               = var.additional_node_pools[count.index].vm_size
-  zones                 = var.availability_zones
   max_pods              = var.max_pods
   os_disk_size_gb       = var.os_disk_size_gb
   tags                  = var.tags
